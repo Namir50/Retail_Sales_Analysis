bc0640638f288e9c4e9bdf313d6025d14851c4b0
Namir50 <namirsayyed50@gmail.com>
2025-06-02 14:19:45 +0530

    Data analysis

--- retail_sales_analysis.sql
+++ retail_sales_analysis.sql
@@ -76,8 +76,36 @@ select count(*) from retail_sales_tb;
 select count(*) as total_sale from retail_sales_tb;
 
 --number of custmers?
-select count(Distinct
-ustomer_id) from retail_sales_tb;
+select count(Distinct customer_id) from retail_sales_tb;
 
 --total sales with respect to gender
 select gender, sum(total_sale) from retail_sales_tb group by gender;
+
+--category or products
+select distinct category from retail_sales_tb;
+
+--DATA ANALYSIS
+
+--Write a SQL query to retrieve all columns for sales made on '2022-11-05:
+select * from retail_sales_tb
+	     where sale_date = '2022-11-05'
+
+--Write a SQL query to retrieve all transactions where the category is 'Clothing' and the quantity sold is more than or equal to 4 in the month of Nov-2022:
+select * from retail_sales_tb
+	     where 
+			category = 'Clothing'
+
+select * from retail_sales_tb
+	     where 
+			quantiy >= 4
+
+select * from retail_sales_tb
+	     where 
+			format(sale_date, 'yyyy-MM') = '2022-11'
+
+--Write a SQL query to calculate the total sales (total_sale) for each category
+select category, sum(total_sale) as totat_sales from retail_sales_tb group by category;
+
+--Write a SQL query to find the average age of customers who purchased items from the 'Beauty' category.:
+select avg(age) from retail_sales_tb 
+				where category = 'Beauty';
